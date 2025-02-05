1ae35a47116e71fdbef69a5911b5163eeadf5057
Johnny Pena <jpenaguitarist@gmaio.com>
2025-02-05 14:50:40 -0600

    Update root.tsx for performance improvements

--- app/root.tsx
+++ app/root.tsx
@@ -26,6 +26,8 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "preload", href: "/assets/root-BmW-Yuzh.css", as: "style" },
+  { rel: "stylesheet", href: "/assets/root-BmW-Yuzh.css" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
